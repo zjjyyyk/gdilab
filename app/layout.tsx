125bb3c5af83030ae7e051b639b8baddcde55173
zjjyyyk <791549989@qq.com>
2025-11-23 00:35:52 +0800

    开启了Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,19 @@ export default function RootLayout({
   return (
     <html lang="zh-CN">
       <head>
+        {/* Microsoft Clarity 跟踪代码 */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ua65g7fdc5");
+            `,
+          }}
+        />
         {/* 预连接地图瓦片服务器,加速地图加载 */}
         <link rel="preconnect" href="https://tile.openstreetmap.org" />
         <link rel="preconnect" href="https://basemaps.cartocdn.com" />
